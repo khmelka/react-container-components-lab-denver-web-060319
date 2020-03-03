ce3c72650c7b3d2fbdac3cd26416f7c2f55e54c8
Nelly <nkhmeleva@yahoo.com>
2020-03-02 23:09:24 -0700

    init

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,13 @@
-// Code MovieReviews Here
+import React from 'react'
+
+export default function MovieReviews({reviews}) {
+    return (
+        <div className="review-list">
+            {reviews.map(review => {
+                return (
+                    review.display_title
+                )
+            })}
+        </div>
+    )
+}
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,28 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+
+export default class SearchableMovieReviewsContainer extends Component {
+
+    state = {
+        reviews: [],
+        searchTerm: []
+    }
+
+    componentDidMount() {
+        fetch(URL)
+        .then(resp => resp.json())
+        .then(reviews => {
+            this.setState({reviews})
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <MovieReviews reviews = {this.state.reviews} />
+            </div>
+        )
+    }
+}
